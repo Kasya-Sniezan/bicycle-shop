b04f62e32c6d4623d841938145ec0633cb36c800
Kasya Sniezan <kasya.sniezan@gmail.com>
2021-10-27 12:22:38 +0300

    add slick-slider

--- #source/js/main.js
+++ #source/js/main.js
@@ -1,3 +1,11 @@
-//@prepros-append slider.js
 //@prepros-append menu-burger.js
 //@prepros-append go-to-block.js
+//@prepros-append slick.min.js
+
+$(document).ready(function () {
+  $(".slick-slider").slick({
+    arrows: false,
+    dots: true,
+    adaptiveHeight: true,
+  });
+});
